acd66762deaac3505585305056ec2bcb50c831d4
xionghoujin <892989054@qq.com>
2018-04-12 16:43:15 +0800

    创建了Operation接口

---
+++ src/main/java/com/bst/red_green_blue/controller/OperationController.java
@@ -0,0 +1,16 @@
+package com.bst.red_green_blue.controller;
+
+import com.bst.red_green_blue.common.ServerResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author XHJ
+ * 2018/4/12 16:37
+ */
+@RestController
+@RequestMapping("/Operation/")
+public class OperationController {
+
+//    public ServerResponse
+}
